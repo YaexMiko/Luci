f3c0d05e1fef2016ad6cd333ad6b1e5815afd76c
YaexMiko <aloksinghmains756@gmail.com>
2025-03-09 16:28:57 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,3 +1,5 @@
+#(©)Javpostr made by @rohit_1888
+
 from bot import Bot
 from pyrogram.types import Message
 from pyrogram import filters
@@ -13,9 +15,7 @@ async def stats(bot: Bot, message: Message):
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
 
 
-@Bot.on_message(filters.private & ~filters.user(ADMINS) & ~filters.command(['start','ping', 'ch2l', 'cancel']))
-# @Bot.on_message(filters.private & filters.incoming)
-async def useless(_, message: Message):
+@Bot.on_message(filters.private)
+async def useless(_,message: Message):
     if USER_REPLY_TEXT:
         await message.reply(USER_REPLY_TEXT)
-
