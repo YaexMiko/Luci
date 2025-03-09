989bb8d179cbf3ab68e19be62f5023a7e914a85d
YaexMiko <aloksinghmains756@gmail.com>
2025-03-09 18:20:15 +0530

    Update join_reqs2.py

--- database/join_reqs2.py
+++ database/join_reqs2.py
@@ -15,38 +15,28 @@ class JoinReqs2:
             self.col = None
 
     def isActive(self):
-        """Check if the database connection is active."""
-        return self.client is not None
+        if self.client is not None:
+            return True
+        else:
+            return False
 
     async def add_user(self, user_id, first_name, username, date):
-        """Add a user to the database."""
         try:
-            await self.col.insert_one({
-                "_id": int(user_id),
-                "user_id": int(user_id),
-                "first_name": first_name,
-                "username": username,
-                "date": date
-            })
+            await self.col.insert_one({"_id": int(user_id), "user_id": int(user_id), "first_name": first_name, "username": username, "date": date})
         except:
             pass
 
     async def get_user(self, user_id):
-        """Retrieve a user from the database."""
         return await self.col.find_one({"user_id": int(user_id)})
 
     async def get_all_users(self):
-        """Retrieve all users from the database."""
         return await self.col.find().to_list(None)
 
     async def delete_user(self, user_id):
-        """Delete a user from the database."""
         await self.col.delete_one({"user_id": int(user_id)})
 
     async def delete_all_users(self):
-        """Delete all users from the database."""
         await self.col.delete_many({})
 
     async def get_all_users_count(self):
-        """Get the total count of users in the database."""
         return await self.col.count_documents({})
