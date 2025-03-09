a0af3ac120649546e4c2d6c87cb2543ea15dafc3
YaexMiko <aloksinghmains756@gmail.com>
2025-03-09 13:39:01 +0530

    Create route.py

---
+++ plugins/route.py
@@ -0,0 +1,9 @@
+#Credit- Telegram @StupidBoi69
+
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("StupidBoi")
