de58d52e63b400c96716e70bff812ba28a4fa609
YaexMiko <aloksinghmains756@gmail.com>
2025-03-09 17:32:56 +0530

    Update join_reqs.py

--- database/join_reqs.py
+++ database/join_reqs.py
@@ -1,10 +1,8 @@
-#(©)Javpostr made by @rohit_1888
-
+# (©)Javpostr made by @rohit_1888
 import motor.motor_asyncio
 from config import FORCE_SUB_CHANNEL
 
 class JoinReqs:
-  
     def __init__(self):
         from config import JOIN_REQS_DB
         if JOIN_REQS_DB:
@@ -16,30 +14,35 @@ class JoinReqs:
             self.db = None
             self.col = None
 
-        def isActive(self):
+    def isActive(self):
         if self.client is not None:
             return True
         else:
             return False
 
-      async def add_user(self, user_id, first_name, username, date):
+    async def add_user(self, user_id, first_name, username, date):
         try:
-            await self.col.insert_one({"_id": int(user_id),"user_id": int(user_>
+            await self.col.insert_one({
+                "_id": int(user_id),
+                "user_id": int(user_id),
+                "first_name": first_name,
+                "username": username,
+                "date": date
+            })
         except:
             pass
 
-
     async def get_user(self, user_id):
         return await self.col.find_one({"user_id": int(user_id)})
-                                                                          
+
     async def get_all_users(self):
         return await self.col.find().to_list(None)
-                                                                          
+
     async def delete_user(self, user_id):
         await self.col.delete_one({"user_id": int(user_id)})
-                                                                          
+
     async def delete_all_users(self):
         await self.col.delete_many({})
-                                                                          
+
     async def get_all_users_count(self):
         return await self.col.count_documents({})
